d403db37328da33c46e87e78162f8a35e2106995
jcsilverx <jcsilverx@gmail.com>
2024-03-02 09:42:04 -0500

    Updated

--- src/components/dialog/dialog-content.tsx
+++ src/components/dialog/dialog-content.tsx
@@ -1,10 +1,11 @@
 type DialogContentProps = {
   children: React.ReactNode;
+  className?: string;
 };
 
 export default function DialogContent({ children }: DialogContentProps) {
   return (
-    <div className='relative flex flex-col w-full text-inherit pointer-events-auto bg-white border border-gray-950/90 rounded-lg shadow outline-[0]'>
+    <div className="relative flex flex-col w-full text-inherit pointer-events-auto bg-white border border-gray-950/90 rounded-lg shadow outline-[0]">
       {children}
     </div>
   );
