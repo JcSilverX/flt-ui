f961f342e74528720e4e235744d809fe8ca02644
jcsilverx <jcsilverx@gmail.com>
2024-03-03 11:00:28 -0500

    Updated

--- src/lib/hooks/use-focus-trap.ts
+++ src/lib/hooks/use-focus-trap.ts
@@ -11,91 +11,91 @@ type TFocusDirection = "backward" | "forward";
  * @param autoFocus
  */
 export default function useFocusTrap(
-	/**
-	 * References the target element.
-	 */
-	ref: React.RefObject<HTMLElement>,
-	/**
-	 * Indicates whether the target is visible.
-	 */
-	isOpen: boolean,
-	/**
-	 * Specifies the index of the focusable child to automatically focus on mount.
-	 */
-	autoFocus = 0
+  /**
+   * References the target element.
+   */
+  ref: React.RefObject<HTMLElement>,
+  /**
+   * Indicates whether the target is visible.
+   */
+  isOpen: boolean,
+  /**
+   * Specifies the index of the focusable child to automatically focus on mount.
+   */
+  autoFocus = 0
 ) {
-	React.useEffect(() => {
-		const trapElement = ref.current;
+  React.useEffect(() => {
+    const trapElement = ref.current;
 
-		// guard clause
-		if (!trapElement) return;
+    // guard clause
+    if (!trapElement) return;
 
-		const elements = SelectorEngine.focusableChildren(trapElement);
+    const elements = SelectorEngine.focusableChildren(trapElement);
 
-		// event handlers / actions
-		const handleKeyDown = (event: KeyboardEvent): void => {
-			if (event.key !== "Tab") return;
+    // event handlers / actions
+    const handleKeyDown = (event: KeyboardEvent): void => {
+      if (event.key !== "Tab") return;
 
-			// prevent default behaviour
-			event.preventDefault();
+      // prevent default behaviour
+      event.preventDefault();
 
-			const newDirection: TFocusDirection = event.shiftKey
-				? "backward"
-				: "forward";
+      const newDirection: TFocusDirection = event.shiftKey
+        ? "backward"
+        : "forward";
 
-			if (!elements || elements.length === 0) return;
+      if (!elements || elements.length === 0) return;
 
-			const currentActiveIndex = getCurrentActiveIndex(elements);
+      const currentActiveIndex = getCurrentActiveIndex(elements);
 
-			if (newDirection === "backward") {
-				// !!Shift + Tab: move focus backward
-				const prevIndex =
-					currentActiveIndex === 0
-						? elements.length - 1
-						: currentActiveIndex - 1;
+      if (newDirection === "backward") {
+        // !!Shift + Tab: move focus backward
+        const prevIndex =
+          currentActiveIndex === 0
+            ? elements.length - 1
+            : currentActiveIndex - 1;
 
-				elements[prevIndex].focus();
-			} else if (newDirection === "forward") {
-				// !!Tab: move focus forward
-				const nextIndex = (currentActiveIndex + 1) % elements.length;
+        elements[prevIndex].focus();
+      } else if (newDirection === "forward") {
+        // !!Tab: move focus forward
+        const nextIndex = (currentActiveIndex + 1) % elements.length;
 
-				elements[nextIndex].focus();
-			}
-		};
+        elements[nextIndex].focus();
+      }
+    };
 
-		/**
-		 * Returns the index of the currently active element.
-		 * @param elements
-		 * @returns number
-		 */
-		const getCurrentActiveIndex = (elements: NodeListOf<HTMLElement>) => {
-			return Array.from(elements).findIndex(
-				(element) => element === document.activeElement
-			);
-		};
+    /**
+     * Returns the index of the currently active element.
+     * @param elements
+     * @returns number
+     */
+    const getCurrentActiveIndex = (elements: NodeListOf<HTMLElement>) => {
+      return Array.from(elements).findIndex(
+        (element) => element === document.activeElement
+      );
+    };
 
-		const activateFocusTrap = (): void => {
-			if (isOpen) {
-				trapElement.addEventListener("keydown", handleKeyDown);
+    const activateFocusTrap = (): void => {
+      if (isOpen) {
+        trapElement.addEventListener("keydown", handleKeyDown);
 
-				// !!DEFAULT: if no element with autoFocus attribute is found,
-				//            focus is set to the first focusable element.
-				const firstFocusableEl =
-					autoFocus >= 0 ? elements[autoFocus] : elements[0];
+        // !!DEFAULT: if no element with autoFocus attribute is found,
+        //            focus is set to the first focusable element.
+        const firstFocusableEl =
+          autoFocus >= 0 ? elements[autoFocus] : elements[0];
 
-				if (firstFocusableEl) {
-					firstFocusableEl.focus();
-				} else {
-					trapElement.focus();
-				}
-			}
-		};
+        if (firstFocusableEl) {
+          firstFocusableEl.focus();
+        } else {
+          trapElement.focus();
+        }
+      }
+    };
 
-		const deactivateFocusTrap = (): void => {
-			trapElement.removeEventListener("keydown", handleKeyDown);
-		};
+    const deactivateFocusTrap = (): void => {
+      trapElement.removeEventListener("keydown", handleKeyDown);
+    };
 
-		activateFocusTrap();
-		return deactivateFocusTrap;
-	}, [autoFocus, isOpen, ref]);
+    activateFocusTrap();
+    return deactivateFocusTrap;
+  }, [autoFocus, isOpen, ref]);
 }
