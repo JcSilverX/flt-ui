2947280352360fa2d99a689341ea9204c57ffdb2
jcsilverx <jcsilverx@gmail.com>
2024-03-28 16:28:01 -0400

    Updated -- working touch event

--- src/components/carousel/stories/carousel-example1.tsx
+++ src/components/carousel/stories/carousel-example1.tsx
@@ -1,59 +1,109 @@
 "use client";
 
+import Button from "@/components/button/button";
 import cn from "@/lib/utils/cn";
 import { wrap } from "@/lib/utils/wrap";
 import React from "react";
 
+const DRAG_TRESHOLD = 50 as const;
 const SLIDE_COUNT = 5 as const;
 const SLIDES = Array.from({ length: SLIDE_COUNT });
 
 export function CarouselExample1() {
   // state
   const [page, setPage] = React.useState<number>(0);
+  const [isTransitioning, setIsTransitioning] = React.useState(false);
   const carouselContentRef = React.useRef<HTMLDivElement>(null);
-  const { width: clientWidth } = useMeasure(carouselContentRef);
+  // const { width: clientWidth } = useMeasure(carouselContentRef);
+  const [clientWidth, setClientWidth] = React.useState<number>(0);
   const [isDragging, setIsDragging] = React.useState<boolean>(false);
   const [dragStartX, setDragStartX] = React.useState<number>(0);
   const [dragDistance, setDragDistance] = React.useState<number>(0);
+  const [direction, setDirection] = React.useState<"previous" | "next">("next");
 
   // derived state
 
-  // event handlers/actions
+  // event handlers / actions
   const paginate = React.useCallback(
     (newDirection: number): void =>
-      setPage(wrap(0, SLIDES.length as number, page + newDirection)),
+      setPage(wrap(0, SLIDE_COUNT, page + newDirection)),
     [page]
   );
 
-  const handlePointerDown = (
-    event: React.PointerEvent<HTMLDivElement>
-  ): void => {
-    setIsDragging(true);
-    setDragStartX(event.clientX);
-    setDragDistance(0);
-  };
+  const handleNext = React.useCallback((): void => {
+    setIsTransitioning(true);
+    setDirection("next");
+    paginate(1);
+  }, [paginate]);
 
-  const handlePointerMove = (
-    event: React.PointerEvent<HTMLDivElement>
-  ): void => {
-    if (!isDragging) return;
+  const handlePrev = React.useCallback((): void => {
+    setIsTransitioning(true);
+    setDirection("previous");
+    paginate(-1);
+  }, [paginate]);
 
-    event.persist();
+  const handleKeyDown = React.useCallback(
+    (event: React.KeyboardEvent<HTMLDivElement>) => {
+      console.log("key pressed");
+      if (event.key === "ArrowRight") {
+        handleNext();
+      } else if (event.key === "ArrowLeft") {
+        handlePrev();
+      }
+    },
+    [handleNext, handlePrev]
+  );
 
-    const newDistance = event.clientX - dragStartX;
-    setDragDistance(newDistance);
-  };
+  const handlePointerDown = React.useCallback(
+    (event: React.PointerEvent<HTMLDivElement>): void => {
+      setIsDragging(true);
+      setDragStartX(event.clientX);
 
-  const handlePointerUp = (event: React.PointerEvent<HTMLDivElement>): void => {
-    if (dragDistance > 50) {
-      paginate(-1);
-    } else if (dragDistance < -50) {
-      paginate(1);
-    }
+      event.currentTarget.setPointerCapture(event.pointerId);
+      event.preventDefault();
+    },
+    []
+  );
 
-    setIsDragging(false);
-    setDragDistance(0);
-  };
+  const handlePointerMove = React.useCallback(
+    (event: React.PointerEvent<HTMLDivElement>): void => {
+      if (!isDragging) return;
+
+      const newDistance = event.clientX - dragStartX;
+      setDragDistance(newDistance);
+
+      event.preventDefault();
+    },
+    [dragStartX, isDragging]
+  );
+
+  const handlePointerUp = React.useCallback(
+    (event: React.PointerEvent<HTMLDivElement>): void => {
+      setIsDragging(false);
+
+      // setPage(wrap(0, SLIDE_COUNT, page - Math.sign(dragDistance)));
+      if (dragDistance < -DRAG_TRESHOLD) {
+        handleNext();
+      } else if (dragDistance > DRAG_TRESHOLD) {
+        handlePrev();
+      }
+
+      setDragDistance(0);
+
+      event.currentTarget.releasePointerCapture(event.pointerId);
+      event.preventDefault();
+    },
+    [dragDistance, handleNext, handlePrev]
+  );
+
+  // useEffect
+  React.useEffect(() => {
+    const carouselContent = carouselContentRef.current;
+
+    if (!carouselContent) return;
+
+    setClientWidth(carouselContent.clientWidth);
+  }, []);
 
   return (
     <div
@@ -61,23 +111,26 @@ export function CarouselExample1() {
       role="region"
       aria-roledescription="carousel"
     >
-      <div className="overflow-clip">
+      <div className="overflow-cli">
         <div
           ref={carouselContentRef}
           className={cn(
-            "backface-hidden touch-pan-y flex -ml-4 transition-transform duration-500 ease-in-out",
+            "backface-hidden flex touch-pan-y -ml-4 transition-transform duration-500 ease-in-out",
             {
-              "transition-none duration-0 ease-[none]": isDragging,
+              "transition-none duration-0 ease-[none]":
+                isDragging || !isTransitioning,
             }
           )}
           style={{
             transform: `translate3d(${
-              -page * clientWidth! + dragDistance
+              -page * clientWidth + dragDistance
             }px, 0px, 0px)`,
           }}
+          tabIndex={-1}
           onPointerDown={handlePointerDown}
           onPointerMove={handlePointerMove}
           onPointerUp={handlePointerUp}
+          onKeyDown={handleKeyDown}
         >
           {SLIDES.map((_, index) => (
             <div
@@ -97,6 +150,9 @@ export function CarouselExample1() {
           ))}
         </div>
       </div>
+
+      <Button onClick={handlePrev}>{"<"}</Button>
+      <Button onClick={handleNext}>{">"}</Button>
     </div>
   );
 }
@@ -116,144 +172,15 @@ function useMeasure(ref: React.RefObject<HTMLElement>) {
   const [{ width, height }, setSize] = React.useState<TSize>(initialSize);
 
   React.useEffect(() => {
-    const carouselContent = ref.current;
+    const refx = ref.current;
 
-    if (!carouselContent) return;
+    if (!refx) return;
 
-    setSize({
-      width: carouselContent.clientWidth,
-      height: carouselContent.clientHeight,
-    });
+    setSize({ width: refx.clientWidth, height: refx.clientHeight });
   }, [ref]);
 
   return { width, height };
 }
-// import Button, { ButtonProps } from "@/components/button/button";
-// import cn from "@/lib/utils/cn";
-// import { wrap } from "@/lib/utils/wrap";
-// import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons";
-// import React from "react";
-
-// export function CarouselExample1() {
-//   // state
-//   const [page, setPage] = React.useState<number>(0);
-//   const [clientWidth, setClientWidth] = React.useState<number>(0);
-//   const [isDragging, setIsDragging] = React.useState<boolean>(false);
-//   const [dragStartX, setDragStartX] = React.useState<number>(0);
-//   const [dragDistance, setDragDistance] = React.useState<number>(0);
-//   const carouselContentRef = React.useRef<HTMLDivElement>(null);
-//   const childCount = 5;
-
-//   // derived state
-
-//   // event handlers / actions
-// const paginate = React.useCallback(
-//   (newDirection: number): void =>
-//     setPage(wrap(0, childCount, page + newDirection)),
-//   [page]
-// );
-
-//   const handlePrev = React.useCallback((): void => {
-//     paginate(-1);
-//   }, [paginate]);
-
-//   const handleNext = React.useCallback((): void => {
-//     paginate(1);
-//   }, [paginate]);
-
-//   React.useEffect(() => {
-//     const carouselContent = carouselContentRef.current;
-
-//     if (!carouselContent) return;
-
-//     setClientWidth(carouselContent.clientWidth);
-
-//     const firstChild = carouselContent.children[0] as HTMLDivElement;
-
-//     if (page === childCount - 1) {
-//       firstChild.style.transform = `translate3d(${
-//         childCount * clientWidth
-//       }px, 0px, 0px)`;
-//     } else {
-//       firstChild.style.transform = `translate3d(0px, 0px, 0px)`;
-//     }
-//   }, [clientWidth, page]);
-
-//   return (
-//     <div className="w-full max-w-xs mx-auto">
-//       <div className="relative overflow-hidden">
-//         <div
-//           ref={carouselContentRef}
-//           className={cn(
-//             "backface-hidden touch-pan-y flex -ml-4 transition-transform duration-500 ease-in-out",
-// {
-//   "transition-none duration-0 ease-[none]": isDragging,
-// }
-//           )}
-//           style={{
-//             transform: `translate3d(${
-//               -page * clientWidth + dragDistance
-//             }px, 0px, 0px)`,
-//           }}
-//           onMouseDown={(event) => {
-//             setIsDragging(true);
-//             setDragStartX(event.clientX);
-//             setDragDistance(0);
-//           }}
-//           onMouseMove={(event) => {
-//             if (!isDragging) return;
-
-//             console.log("dragging");
-
-//             setDragDistance(event.clientX - dragStartX);
-//           }}
-//           onMouseUp={(event) => {
-//             setIsDragging(false);
-//           }}
-//         >
-//           {Array.from({ length: childCount }).map((_, index) => (
-//             <div
-//               key={index}
-//               className={cn(
-//                 "min-w-0 flex-shrink-0 flex-grow-0 basis-full pl-4",
-//                 {
-//                   active: page === index,
-//                 }
-//               )}
-//               role="group"
-//               aria-roledescription="slide"
-//             >
-// <div className="pl-1">
-//   <div className="rounded-xl border shadow">
-//     <div className="flex aspect-square items-center justify-center p-6">
-//       <span className="text-4xl font-semibold">{index + 1}</span>
-//     </div>
-//   </div>
-// </div>
-//             </div>
-//           ))}
-//         </div>
-
-//         <CarouselControl
-//           onClick={handlePrev}
-//           className="left-0"
-//           aria-label="carousel items"
-//           aria-controls="previous page"
-//         >
-//           <ChevronLeftIcon width={32} height={32} />
-//         </CarouselControl>
-//         <CarouselControl
-//           onClick={handleNext}
-//           className="right-0"
-//           aria-label="carousel items"
-//           aria-controls="next page"
-//         >
-//           <ChevronRightIcon width={32} height={32} />
-//         </CarouselControl>
-//       </div>
-//     </div>
-//   );
-// }
 
 // type CarouselControlProps = ButtonProps;
 
@@ -273,3 +200,30 @@ function useMeasure(ref: React.RefObject<HTMLElement>) {
 //     />
 //   );
 // }
+
+// React.useEffect(() => {
+//   const carouselContent = carouselContentRef.current;
+
+//   if (!carouselContent) return;
+
+//   const firstChild = carouselContent.children[0] as HTMLDivElement;
+//   const lastChild = carouselContent.children[
+//     carouselContent.children.length - 1
+//   ] as HTMLDivElement;
+
+//   if (page === 0 && dragDistance) {
+//     lastChild.style.transform = `translate3d(${
+//       -SLIDE_COUNT * clientWidth
+//     }px, 0px, 0px)`;
+//   } else {
+//     lastChild.style.transform = `translate3d(${0}px, 0px, 0px)`;
+//   }
+
+//   if (page === SLIDE_COUNT - 1 && dragDistance) {
+//     firstChild.style.transform = `translate3d(${
+//       SLIDE_COUNT * clientWidth
+//     }px, 0px, 0px)`;
+//   } else {
+//     firstChild.style.transform = `translate3d(${0}px, 0px, 0px)`;
+//   }
+// }, [clientWidth, dragDistance, page]);
