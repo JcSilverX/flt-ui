7dd071e9bc61a65143940ea679b8640eebb67ae2
jcsilverx <jcsilverx@gmail.com>
2024-04-04 20:26:35 -0400

    Updated

--- src/components/alert/alert.tsx
+++ src/components/alert/alert.tsx
@@ -4,28 +4,9 @@ import React from "react";
 import { VariantProps, cva } from "class-variance-authority";
 import cn from "@/lib/utils/cn";
 import Button from "../button/button";
-import {
-  AnimatePresence,
-  AnimationProps,
-  Transition,
-  Variants,
-  motion,
-} from "framer-motion";
 import AlertDescription from "./alert-description";
 import AlertLink from "./alert-link";
 import AlertTitle from "./alert-title";
-
-const AlertAnimation = {
-  initial: {
-    opacity: 0,
-  },
-  animate: {
-    opacity: 1,
-  },
-  exit: {
-    opacity: 0,
-  },
-};
 
 export function AlertExample1() {
   const [isOpen, setIsOpen] = React.useState(false);
@@ -34,7 +15,7 @@ export function AlertExample1() {
     <>
       <Button onClick={() => setIsOpen((prev) => !prev)}>Open alert</Button>
 
-      <Alert show={isOpen} animation={AlertAnimation}>
+      <Alert show={isOpen}>
         <AlertTitle>Heads up!</AlertTitle>
         <AlertDescription>
           You can add <AlertLink>components</AlertLink> to your app using the
@@ -60,36 +41,31 @@ const AlertVariants = cva(
   }
 );
 
-type Animation = Omit<AnimationProps, "variants" | "transition">;
+export type AlertProps = React.HTMLAttributes<HTMLDivElement> &
+  VariantProps<typeof AlertVariants> & {
+    reference?: React.RefObject<HTMLDivElement>;
+    show?: boolean;
+  };
 
-export type AlertProps = React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof AlertVariants> & {
-  reference?: React.RefObject<HTMLDivElement>;
-  show?: boolean;
-  animation?: false | Animation;
-  transition?: Transition;
-  className?: string;
-};
-
-export default function Alert({ children, show, animation, transition, className, variant, ...props }: AlertProps) {
+export default function Alert({
+  show,
+  variant,
+  className,
+  ...props
+}: AlertProps) {
   const { reference: ref } = props;
 
   return (
-    <AnimatePresence>
+    <>
       {show && (
-        <motion.div
+        <div
           key={"alert"}
           ref={ref}
           role="alert"
-          variants={animation === false ? {} : animation as Variants}
-          initial={"initial"}
-          animate={"animate"}
-          exit={"exit"}
-          transition={transition}
           className={cn(AlertVariants({ variant, className }))}
-        >
-          {children}
-        </motion.div>
+          {...props}
+        />
       )}
-    </AnimatePresence>
+    </>
   );
 }
