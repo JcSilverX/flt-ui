a95055a85832c13f09b5a6411312a5a44f796fc3
JcSilverX <jcsilverx@gmail.com>
2024-05-23 06:37:46 -0400

    Updated

--- src/components/sheet/sheet.tsx
+++ src/components/sheet/sheet.tsx
@@ -1,20 +1,40 @@
+"use client";
+
 import React from "react";
 import Button, { ButtonProps } from "../button/button";
+import cn from "@/lib/utils/cn";
+import { Cross2Icon } from "@radix-ui/react-icons";
+import useIsMounted from "@/lib/hooks/use-is-mounted";
+import { createPortal } from "react-dom";
 
 type SheetProps = React.HTMLAttributes<HTMLDivElement> & {
 	reference?: React.RefObject<HTMLDivElement>;
 };
 
-export default function Sheet({ ...props }: SheetProps) {
-	return <div {...props} />;
+export default function Sheet({ className, ...props }: SheetProps) {
+	const { reference: ref } = props;
+
+	return <div ref={ref} className={cn("", className, {})} {...props} />;
 }
 
 type SheetOverlayProps = React.HTMLAttributes<HTMLDivElement> & {
 	reference?: React.RefObject<HTMLDivElement>;
 };
 
-export function SheetOverlay({ ...props }: SheetOverlayProps) {
-	return <div {...props} />;
+export function SheetOverlay({ className, ...props }: SheetOverlayProps) {
+	const { reference: ref } = props;
+
+	return (
+		<div
+			ref={ref}
+			className={cn(
+				"fixed inset-0 z-50 bg-black/80 data-[jsx-state=open]:| data-[jsx-state=closed]:| data-[jsx-state=open]:| data-[jsx-state=closed]:|",
+				className,
+				{}
+			)}
+			{...props}
+		/>
+	);
 }
 
 type SheetTriggerProps = ButtonProps;
@@ -33,24 +53,51 @@ type SheetHeaderProps = React.HTMLAttributes<HTMLDivElement> & {
 	reference?: React.RefObject<HTMLDivElement>;
 };
 
-export function SheetHeader({ ...props }: SheetHeaderProps) {
-	return <div {...props} />;
+export function SheetHeader({ className, ...props }: SheetHeaderProps) {
+	const { reference: ref } = props;
+
+	return (
+		<div
+			ref={ref}
+			className={cn("flex flex-col text-center sm:text-left", className, {})}
+			{...props}
+		/>
+	);
 }
 
-type SheetTitleProps = React.HTMLAttributes<HTMLDivElement> & {
-	reference?: React.RefObject<HTMLDivElement>;
+type SheetTitleProps = React.HTMLAttributes<HTMLHeadingElement> & {
+	reference?: React.RefObject<HTMLHeadingElement>;
 };
 
-export function SheetTitle({ ...props }: SheetTitleProps) {
-	return <div {...props} />;
+export function SheetTitle({ className, ...props }: SheetTitleProps) {
+	const { reference: ref } = props;
+
+	return (
+		<h2
+			ref={ref}
+			className={cn("text-lg font-semibold text-gray-950", className, {})}
+			{...props}
+		/>
+	);
 }
 
-type SheetDescriptionProps = React.HTMLAttributes<HTMLDivElement> & {
-	reference?: React.RefObject<HTMLDivElement>;
+type SheetDescriptionProps = React.HTMLAttributes<HTMLParagraphElement> & {
+	reference?: React.RefObject<HTMLParagraphElement>;
 };
 
-export function SheetDescription({ ...props }: SheetDescriptionProps) {
-	return <div {...props} />;
+export function SheetDescription({
+	className,
+	...props
+}: SheetDescriptionProps) {
+	const { reference: ref } = props;
+
+	return (
+		<p
+			ref={ref}
+			className={cn("text-sm text-gray-700", className, {})}
+			{...props}
+		/>
+	);
 }
 
 type SheetContentProps = React.HTMLAttributes<HTMLDivElement> & {
@@ -61,18 +108,73 @@ type SheetPortalProps = React.HTMLAttributes<HTMLDivElement> & {
 	reference?: React.RefObject<HTMLDivElement>;
 };
 
-export function SheetPortal({ ...props }: SheetPortalProps) {
-	return <div {...props} />;
+export function SheetPortal({ className, ...props }: SheetPortalProps) {
+	const { reference: ref } = props;
+	const isMounted = useIsMounted();
+
+	return (
+		isMounted &&
+		createPortal(
+			<div ref={ref} className={cn("", className, {})} {...props} />,
+			document.body
+		)
+	);
 }
 
-export function SheetContent({ ...props }: SheetContentProps) {
-	return <div {...props} />;
+export function SheetContent({
+	children,
+	className,
+	...props
+}: SheetContentProps) {
+	const { reference: ref } = props;
+
+	return (
+		<SheetPortal>
+			<SheetOverlay>
+				<div
+					ref={ref}
+					className={cn(
+						"fixed z-50 bg-gray-50 p-6 shadow-lg transition ease-in-out data-[jsx-state=open]:| data-[jsx-state=closed]:| data-[jsx-state=open]:| data-[jsx-state=closed]:|",
+						className,
+						{
+							"inset-y-0 right-0 h-full w-3/4 border-l data-[jsx-state=open]:| data-[jsx-state=closed]:| sm:max-w-sm":
+								true,
+						}
+					)}
+					{...props}
+				>
+					{children}
+
+					<SheetClose
+						variant={"ghost"}
+						size={"icon"}
+						className={cn("absolute top-4 right-4 rounded-sm opacity-70")}
+					>
+						<Cross2Icon height={16} width={16} />
+						<span className="sr-only">Close</span>
+					</SheetClose>
+				</div>
+			</SheetOverlay>
+		</SheetPortal>
+	);
 }
 
 type SheetFooterProps = React.HTMLAttributes<HTMLDivElement> & {
 	reference?: React.RefObject<HTMLDivElement>;
 };
 
-export function SheetFooter({ ...props }: SheetFooterProps) {
-	return <div {...props} />;
+export function SheetFooter({ className, ...props }: SheetFooterProps) {
+	const { reference: ref } = props;
+
+	return (
+		<div
+			ref={ref}
+			className={cn(
+				"flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2",
+				className,
+				{}
+			)}
+			{...props}
+		/>
+	);
 }
--- src/components/sheet/stories/sheet.stories.tsx
+++ src/components/sheet/stories/sheet.stories.tsx
@@ -1,19 +1,54 @@
 import React from "react";
 import Sheet, {
+	SheetClose,
 	SheetContent,
 	SheetDescription,
+	SheetFooter,
 	SheetHeader,
 	SheetTitle,
+	SheetTrigger,
 } from "../sheet";
+import Label from "@/components/label/label";
+import Input from "@/components/input/input";
 
 export default function SheetDemo() {
 	return (
 		<Sheet>
-			<SheetHeader>
-				<SheetTitle></SheetTitle>
-				<SheetDescription></SheetDescription>
-			</SheetHeader>
-			<SheetContent></SheetContent>
+			<SheetTrigger variant={"outline"}>Open</SheetTrigger>
+
+			<SheetContent>
+				<SheetHeader>
+					<SheetTitle>Edit profile</SheetTitle>
+					<SheetDescription>
+						Make changes to your profile here. Click save when you{"'"}re done.
+					</SheetDescription>
+				</SheetHeader>
+				<div className="grid gap-4 py-4">
+					<div className="grid grid-cols-4 items-center gap-4">
+						<Label htmlFor="name" className="text-right">
+							Name
+						</Label>
+						<Input
+							id="name"
+							placeholder="Pedro Duarte"
+							className="col-span-3"
+						/>
+					</div>
+					<div className="grid grid-cols-4 items-center gap-4">
+						<Label htmlFor="username" className="text-right">
+							Username
+						</Label>
+						<Input
+							id="username"
+							placeholder="@peduarte"
+							className="col-span-3"
+						/>
+					</div>
+				</div>
+				<SheetFooter>
+					<SheetClose type="submit">Save changes</SheetClose>
+				</SheetFooter>
+			</SheetContent>
 		</Sheet>
 	);
 }
